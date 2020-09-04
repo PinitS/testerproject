d9db025bfb514a7a559cda6f0ac3c3b196689191
PinitS <zproject.4.u@gmail.com>
2020-09-04 11:08:05 +0700

    second commit

---
+++ app/Http/Controllers/HashtagController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\hashtag;
+use Illuminate\Http\Request;
+
+class HashtagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\hashtag  $hashtag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(hashtag $hashtag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\hashtag  $hashtag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(hashtag $hashtag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\hashtag  $hashtag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, hashtag $hashtag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\hashtag  $hashtag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(hashtag $hashtag)
+    {
+        //
+    }
+}
---
+++ database/factories/HashtagFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\hashtag;
+use Faker\Generator as Faker;
+
+$factory->define(hashtag::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
